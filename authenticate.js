5f39fd3b5f5e6d586e7b9b661f9da9eda277f537
Daniel <79483260+InfiniteTurbo@users.noreply.github.com>
2023-03-10 17:08:22 -0500

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,22 @@
+const users = require('./initialData').users; // import initial data
+
 const genAPIKey = () => {
   //create a base-36 string that contains 30 chars in a-z,0-9
   return [...Array(30)]
     .map((e) => ((Math.random() * 36) | 0).toString(36))
     .join('');
+};
+
+const createUser = (_username, req) => {
+  let today = new Date().toISOString().split('T')[0];
+  let user = {
+    _id: Date.now(),
+    api_key: genAPIKey(),
+    username: _username,
+    usage: [{ date: today, count: 0 }],
+  };
+
+  console.log('add user');
+  users.push(user);
+  return user;
 };
